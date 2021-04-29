3e59ce691c0319d43e0e1c7a7e4324ac544256c3
vladislavirkin <vladislavirkin@outlook.com>
2021-04-29 20:52:18 +0300

    feature(app): add solution

---
+++ converterToASCII/ConverterBitmapToAscii.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace converterToASCII
+{
+    public class ConverterBitmapToAscii
+    {
+        private readonly char[] _asiiTable = { '.', ',', ':', '+', '*', '?', '%', '$', '#', '@' };
+        private readonly char[] _asiiTableNegative = { '@', '#', '$', '%', '?', '*', '+', ':', ',', '.' };
+        private readonly Bitmap _bitmap;
+
+        public char[][] ConvertAsNegative()
+        {
+            return Convert(_asiiTableNegative);
+        }
+
+        public char[][] Convert()
+        {
+            return Convert(_asiiTable);
+        }
+
+        private char[][] Convert(char[] asciiTable)
+        {
+            var result = new char[_bitmap.Height][];
+
+            for (int y = 0; y < _bitmap.Height; y++)
+            {
+                result[y] = new char[_bitmap.Width];
+
+                for (int x = 0; x < _bitmap.Width; x++)
+                {
+                    int mapIndex = (int)Map(_bitmap.GetPixel(x, y).R, 0, 255, 0, asciiTable.Length - 1);
+                    result[y][x] = asciiTable[mapIndex];
+                }
+            }
+
+            return result;
+        }
+
+        private float Map(float valueToMap, float start1, float stop1, float start2, float stop2)
+        {
+            return ((valueToMap - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+        }
+
+        public ConverterBitmapToAscii(Bitmap bitmap)
+        {
+            _bitmap = bitmap;
+        }
+    }
+}
---
+++ converterToASCII/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace converterToASCII
+{
+    class Program
+    {
+        private const double WIDTH_OFFSET = 1.7;
+        private const int MAX_WIDTH = 474;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Images | *.bmp; *.png; *.jpg; *.JPEG"
+            };
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Press enter to start...\n");
+
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+
+                if (cmd == "exit")
+                {
+                    break;
+                }
+
+                if (openFileDialog.ShowDialog() != DialogResult.OK)
+                {
+                    continue;
+                }
+
+                Console.Clear();
+
+                var bitmap = new Bitmap(openFileDialog.FileName);
+                ResizeBitmap(bitmap);
+                bitmap.ToGrayScale();
+
+                var converter = new ConverterBitmapToAscii(bitmap);
+                var rows = converter.Convert();
+
+                foreach (var row in rows)
+                {
+                    Console.WriteLine(row);
+                }
+
+
+                var rowsNegative = converter.ConvertAsNegative();
+                File.WriteAllLines("image.txt", rowsNegative.Select(r => new string(r)));  
+
+                Console.SetCursorPosition(0, 0);
+            }
+        }
+
+        private static Bitmap ResizeBitmap(Bitmap bitmap)
+        {          
+            var newHeight = bitmap.Height / WIDTH_OFFSET * MAX_WIDTH / bitmap.Width;
+            if (bitmap.Width > MAX_WIDTH || bitmap.Height > newHeight)
+            {
+                bitmap = new Bitmap(bitmap, new Size(MAX_WIDTH, (int)newHeight));
+            }
+            return bitmap;
+        }
+
+    }
+}
